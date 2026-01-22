6394741c2ee693918cc714c036d814bfa941558f
mrkirthi-24 <kirthibagrecha4@gmail.com>
2026-01-23 04:05:12 +0530

    fix cors

--- server.py
+++ server.py
@@ -19,7 +19,7 @@ app = FastAPI(title="Voice Agent API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:5173"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
